60395157457bfa27cc6c2508632854dd7916ba16
Cangyu Lin <jefflin.je598@gmail.com>
2023-11-22 22:25:07 +0800

    Update kaishaku_scraper.py

--- kaishaku_scraper.py
+++ kaishaku_scraper.py
@@ -2,7 +2,42 @@ from bs4 import BeautifulSoup
 import requests
 
 # List of query words
-query_texts = ["word1", "word2", "word3"]
+query_texts = [
+"旅順",
+"促して",
+"蒲鉾",
+"女連",
+"結城紬",
+"所々",
+"師走",
+"ポカポカ殴る",
+"べんべら",
+"年の暮れ",
+"羽織",
+"紋付羽織",
+"転じる",
+"偏屈",
+"格子",
+"迂遠",
+"傍らに",
+"端書",
+"春着",
+"エピクテタス",
+"詰腹",
+"突きつける",
+"顧みる",
+"対句",
+"でんぷん質",
+"死骸",
+"焦げ爛れた",
+"代えて",
+"雑煮",
+"お櫃",
+"甞める",
+"吹聴",
+"仕付",
+"間食"
+]
 
 # Iterate over the query words
 for i in range(len(query_texts)):
@@ -12,8 +47,11 @@ for i in range(len(query_texts)):
 
     # Find the main content div
     content_div = soup.find('div', class_='kijiWrp')
+    try:
+        # Extract the text from the content div
+        content_text = content_div.get_text()
 
-    # Extract the text from the content div
-    content_text = content_div.get_text()
+        print(content_text)
 
-    print(content_text)
+    except:
+        print(query_texts[i]+"not found")
